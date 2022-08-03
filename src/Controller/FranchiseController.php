65e303ec5209a6a87c5f9f8c7b13ad51e80e1120
Jolan95 <jolan.aubry@hotmail.fr>
2022-08-04 01:57:49 +0200

    read-only handle access

--- src/Controller/FranchiseController.php
+++ src/Controller/FranchiseController.php
@@ -20,21 +20,32 @@ class FranchiseController extends AbstractController
      */
     public function franchise($token, UserRepository $userRepository): Response
     {
-        if( $this->getUser()->getUrl() === $token){
 
+        if( $this->getUser()->getUrl() === $token){
+            
             $user = $userRepository->findOneBy(["url" => $token]);
             $franchise = $user->getFranchise();   
             $permit = $franchise->getPermit();
-            
-            return $this->render('read-only/franchise.html.twig', [
-                'franchise' => $franchise,
-                "permit" => $permit
-            ]);
+
+            // access only if franchise is valid;
+            if($franchise->isIsActive()){
+
+                return $this->render('read-only/franchise.html.twig', [
+                    'franchise' => $franchise,
+                    "permit" => $permit
+                ]);
+
+            } else {
+
+                throw new Exception("Votre structure est actuellement désactivé.", 403);
+            }
+
         } else{
             // error if the franchise doesn't belong to the user
             throw new Exception("Vous n'avez pas accès à cette page", 500);
         }
     }
+
     /**
      * 
      * @Route("/structure/{token}", name="read_structure")
@@ -43,6 +54,7 @@ class FranchiseController extends AbstractController
     {
         $user = $this->getUser();
         $role = $user->getRoles();
+
         // url of the user connected
         $url = $user->getUrl();
         // entity user corresponding to url token
@@ -50,32 +62,38 @@ class FranchiseController extends AbstractController
         // structure of the token
         $structure = $structureRepository->findOneBy(["id" => $userToken->getStructure()->getId()]);
         $permit = $structure->getPermit();
-        
-        
-        
-        
-        
+        $franchise = $structure ->getFranchise();
+
+        // error if structure or structure's franchise is not valid
+        if(!$structure->isIsActive() || $structure->isIsActive()){
+            throw new Exception("Cette structure est actuellement désactivé.", 403);
+        }
+
         // find the user role
         if (in_array("ROLE_STRUCTURE", $role)){
             // if structure is the user's structure
             if($user->getUrl() === $token){   
+
                 return $this->render('read-only/structure.html.twig', [
                     'structure' => $user->getStructure(),
                     'permit' => $permit
                 ]); 
+                
             }
-            throw new Exception ("Cette page ne correspond pas à votre structure", 500);
+            throw new Exception ("Cette page ne correspond pas à votre structure", 403);
 
-        } else if(in_array("ROLE_FRANCHISE", $role)){
+        } else if (in_array("ROLE_FRANCHISE", $role)){
                 //  if the structure belongs to the user's franchise
-                if($structure->getFranchise()->getId() === $user->getFranchise()->getId());
-                return $this->render('read-only/structure.html.twig', [
-                    'structure' => $structure,
-                    "url" => $url,
-                    "permit" => $permit
-                ]);
+                if($structure->getFranchise()->getId() === $user->getFranchise()->getId()){
+
+                    return $this->render('read-only/structure.html.twig', [
+                        'structure' => $structure,
+                        "url" => $url,
+                        "permit" => $permit
+                    ]);
+                }    
                 //error if structure doesn't belong to the franchise user
-                throw new Exception ("Cette structure ne correspond pas à votre franchise", 500);
+                throw new Exception ("Cette structure ne correspond pas à votre franchise", 403);
             // if user is not franchise or structure
         }
         // if the user is not a structure or  a franchise
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,19 +14,30 @@ class SecurityController extends AbstractController
 
 
     #[Route(path: '/', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
-
-    
+    public function login(AuthenticationUtils $authenticationUtils)
     {
+            
         if ($this->getUser()) {
             
             $user = $this->getUser();
             $roles = $user->getRoles();
+            $structure = $user->getStructure();
+            $franchise = $user->getFranchise();
+
+
+
             if(in_array("ROLE_ADMIN", $roles)){
                 return $this->redirectToRoute('app_admin');
             } else if (in_array("ROLE_STRUCTURE", $roles)){
+                if(!$structure->isIsActive()){
+                    return $this->render('read-only/acces-denied.html.twig', [ 'error' => "Votre structure est suspendu, Fitn'go a suspendu votre accès au site."]);
+                } 
                 return $this->redirectToRoute('read_structure', ["token" =>$user->getUrl()]);
+                
             } else{
+                if(!$franchise->isIsActive()){
+                    return $this->render('read-only/acces-denied.html.twig', [ 'error' => "Votre franchise est suspendu, Fitn'go a suspendu votre accès au site."]);
+                }
                 return $this->redirectToRoute("read_franchise", ["token" => $user->getUrl()]);
             }
             
